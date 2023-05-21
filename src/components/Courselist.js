9f23ec493ae88164be9eaa6af9f1af367b4edf83
godugubalesabu@gmail.com <godugubalesabu@gmail.com>
2023-05-22 00:45:40 +0530

    Initial commit

---
+++ src/components/Courselist.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+const CourseList = () => {
+    const [courses, setCourses] = useState([]);
+  
+    useEffect(() => {
+      // Create a Supabase client
+      const supabase = createClient('YOUR_SUPABASE_URL', 'YOUR_SUPABASE_PUBLIC_KEY');
+  
+      // Fetch the courses from the database
+      const fetchCourses = async () => {
+        const { data, error } = await supabase.from('courses').select('*');
+        if (error) {
+          console.error('Error fetching courses:', error);
+        } else {
+          setCourses(data);
+        }
+      };
+  
+      fetchCourses();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Course List</h1>
+        <ul>
+          {courses.map((course) => (
+            <li key={course.id}>{course.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default CourseList;
+  
